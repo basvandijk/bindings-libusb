f9ec61454908bb42b07172d29a87b3c14dd6ad49
Maurício CA <none@none>
2009-11-16 18:37:27 -0200

    Added missing C code.

--- src/core.c
+++ src/core.c
@@ -25,67 +25,109 @@ struct libusb_device {
  struct libusb_context *ctx;
 };
 
+enum usbi_log_level {
+ LOG_LEVEL_DEBUG,
+ LOG_LEVEL_INFO,
+ LOG_LEVEL_WARNING,
+ LOG_LEVEL_ERROR,
+};
+
+
 static const struct libusb_endpoint_descriptor *find_endpoint(
  struct libusb_config_descriptor *, unsigned char);
 
-void usbi_log(libusb_context*,int,const char*,const char*,...);
+void usbi_log(struct libusb_context *ctx, enum usbi_log_level level,
+ const char *function, const char *format, ...)
+{
+ va_list args;
+ FILE *stream = stdout;
+ const char *prefix;
 
-#ifndef libusb_get_max_iso_packet_size
+ switch (level) {
+ case LOG_LEVEL_INFO:
+  prefix = "info";
+  break;
+ case LOG_LEVEL_WARNING:
+  stream = stderr;
+  prefix = "warning";
+  break;
+ case LOG_LEVEL_ERROR:
+  stream = stderr;
+  prefix = "error";
+  break;
+ case LOG_LEVEL_DEBUG:
+  stream = stderr;
+  prefix = "debug";
+  break;
+ default:
+  stream = stderr;
+  prefix = "unknown";
+  break;
+ }
 
-   int libusb_get_max_iso_packet_size(libusb_device *dev,
-    unsigned char endpoint)
-   {
-    struct libusb_config_descriptor *config;
-    const struct libusb_endpoint_descriptor *ep;
-    enum libusb_transfer_type ep_type;
-    uint16_t val;
-    int r;
+ fprintf(stream, "libusb:%s [%s] ", prefix, function);
 
-    r = libusb_get_active_config_descriptor(dev, &config);
-    if (r < 0) {
-     usbi_log((dev)->ctx, 3, __FUNCTION__, "could not retrieve active config descriptor");
-     return LIBUSB_ERROR_OTHER;
-    }
+ va_start (args, format);
+ vfprintf(stream, format, args);
+ va_end (args);
 
-    ep = find_endpoint(config, endpoint);
-    if (!ep)
-     return LIBUSB_ERROR_NOT_FOUND;
+ fprintf(stream, "\n");
+}
 
-    val = ep->wMaxPacketSize;
-    ep_type = ep->bmAttributes & 0x3;
-    libusb_free_config_descriptor(config);
+int libusb_get_max_iso_packet_size(libusb_device *dev,
+ unsigned char endpoint)
+{
+ struct libusb_config_descriptor *config;
+ const struct libusb_endpoint_descriptor *ep;
+ enum libusb_transfer_type ep_type;
+ uint16_t val;
+ int r;
 
-    r = val & 0x07ff;
-    if (ep_type == LIBUSB_TRANSFER_TYPE_ISOCHRONOUS
-      || ep_type == LIBUSB_TRANSFER_TYPE_INTERRUPT)
-     r *= (1 + ((val >> 11) & 3));
-    return r;
+ r = libusb_get_active_config_descriptor(dev, &config);
+ if (r < 0) {
+  usbi_log((dev)->ctx, 3, __FUNCTION__, "could not retrieve active config descriptor");
+  return LIBUSB_ERROR_OTHER;
+ }
+
+ ep = find_endpoint(config, endpoint);
+ if (!ep)
+  return LIBUSB_ERROR_NOT_FOUND;
+
+ val = ep->wMaxPacketSize;
+ ep_type = ep->bmAttributes & 0x3;
+ libusb_free_config_descriptor(config);
+
+ r = val & 0x07ff;
+ if (ep_type == LIBUSB_TRANSFER_TYPE_ISOCHRONOUS
+   || ep_type == LIBUSB_TRANSFER_TYPE_INTERRUPT)
+  r *= (1 + ((val >> 11) & 3));
+ return r;
+}
+
+static const struct libusb_endpoint_descriptor *find_endpoint(
+ struct libusb_config_descriptor *config, unsigned char endpoint)
+{
+ int iface_idx;
+ for (iface_idx = 0; iface_idx < config->bNumInterfaces; iface_idx++) {
+  const struct libusb_interface *iface = &config->interface[iface_idx];
+  int altsetting_idx;
+
+  for (altsetting_idx = 0; altsetting_idx < iface->num_altsetting;
+    altsetting_idx++) {
+   const struct libusb_interface_descriptor *altsetting
+    = &iface->altsetting[altsetting_idx];
+   int ep_idx;
+
+   for (ep_idx = 0; ep_idx < altsetting->bNumEndpoints; ep_idx++) {
+    const struct libusb_endpoint_descriptor *ep =
+     &altsetting->endpoint[ep_idx];
+    if (ep->bEndpointAddress == endpoint)
+     return ep;
    }
+  }
+ }
+ return NULL;
+}
 
-   static const struct libusb_endpoint_descriptor *find_endpoint(
-    struct libusb_config_descriptor *config, unsigned char endpoint)
-   {
-    int iface_idx;
-    for (iface_idx = 0; iface_idx < config->bNumInterfaces; iface_idx++) {
-     const struct libusb_interface *iface = &config->interface[iface_idx];
-     int altsetting_idx;
 
-     for (altsetting_idx = 0; altsetting_idx < iface->num_altsetting;
-       altsetting_idx++) {
-      const struct libusb_interface_descriptor *altsetting
-       = &iface->altsetting[altsetting_idx];
-      int ep_idx;
-
-      for (ep_idx = 0; ep_idx < altsetting->bNumEndpoints; ep_idx++) {
-       const struct libusb_endpoint_descriptor *ep =
-        &altsetting->endpoint[ep_idx];
-       if (ep->bEndpointAddress == endpoint)
-        return ep;
-      }
-     }
-    }
-    return NULL;
-   }
-
-#endif
 
